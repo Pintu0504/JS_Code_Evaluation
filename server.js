410ee99f4e852472c4ab6bb45632ae2a105d2f9c
Pintu Kumar <pin205kmr@gmail.com>
2022-08-19 17:16:06 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.use('/', express.static(__dirname + '/public'))
+
+function decodeData(req, res, next) {
+    for (let q in req.query) {
+        let data = req.query[q] 
+        data = Buffer.from(data, 'base64').toString('ascii')
+        req.query[q] = data
+    }
+    next()
+}
+
+app.get('/eval', decodeData, (req, res)=>{
+    let ans = eval(req.query.code);
+    res.send(`The output is \n` + ans)
+})
+
+app.listen(3333, ()=>{
+    console.log('Server started on http://localhost:3333')
+})
+
